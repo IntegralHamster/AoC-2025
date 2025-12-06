049eea93709e94a897b5a38e4a0d76b79379b04d
IntegralHamster <75609426+IntegralHamster@users.noreply.github.com>
2025-12-06 14:31:28 +0200

    Create 2025-6.py

---
+++ 2025-6.py
@@ -0,0 +1,59 @@
+import copy
+
+with open('input.txt') as f:
+    lines = [line for line in f.readlines()]
+
+lines_updated = []
+for line in lines:
+  lines_updated.append(line.split())
+  
+total_p1 = 0  
+for i in range(len(lines_updated[0])):
+  if lines_updated[-1][i] == '+':
+    vertical_total = 0
+    for j in range(len(lines_updated)-1):
+      vertical_total += int(lines_updated[j][i])
+  elif lines_updated[-1][i] == '*':
+    vertical_total = 1
+    for j in range(len(lines_updated)-1):
+      vertical_total *= int(lines_updated[j][i])
+  total_p1 += vertical_total
+
+lines_updated2 = []
+for i in range(len(lines[0])):
+  vertical = ''
+  if '\n' == lines[0][i]:
+    break
+  for j in range(len(lines)-1):
+    vertical += lines[j][i]
+  lines_updated2.append(vertical.replace(' ', ''))
+  
+total_p2 = 0  
+j = 0
+for i in range(len(lines_updated[0])):
+  if lines_updated[-1][i] == '+':
+    horizontal_total = 0
+    while True:
+      if j == len(lines_updated2):
+        break
+      elif lines_updated2[j] == '':
+        j += 1
+        break
+      else:
+        horizontal_total += int(lines_updated2[j])
+        j += 1
+        
+  elif lines_updated[-1][i] == '*':
+    horizontal_total = 1
+    while True:
+      if j == len(lines_updated2):
+        break
+      elif lines_updated2[j] == '':
+        j += 1
+        break
+      else:
+        horizontal_total *= int(lines_updated2[j])
+        j += 1
+  total_p2 += horizontal_total
+  
+print(total_p1, total_p2)
